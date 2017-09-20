ee06f7ea4125078fa364652b137a1c9a6b0e2ee9
Abhijeet Yaduwanshi <ayaduwanshi@nexient.com>
2017-09-20 17:51:06 -0400

    JavaScriptTutorial project added T4

---
+++ JavaScriptTutorial/T1/4.js
@@ -0,0 +1,19 @@
+// declaring a variables
+var firstName; // declares variable in the current scope.
+var firstName, lastName = "Script", address, city;
+
+// log a variable to console
+var firstName;
+console.log(firstName);
+
+// check the datatype of a variable
+var firstName;
+console.log(typeof firstName); // prints undefined to the console
+
+// assigning the variable to undefined
+var firstName = undefined;
+console.log(typeof firstName); // prints undefined to the console
+
+// assigning the variable to null
+var firstName = null;
+console.log(typeof firstName); // prints object to the console
